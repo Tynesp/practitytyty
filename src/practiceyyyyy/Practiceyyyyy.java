dc3dbf64da51eaebc43cc7329ef69f4b202f84df
Tynesp <nicollti@sheridancollege.ca>
2023-07-06 11:56:28 -0400

    ily

---
+++ src/practiceyyyyy/Practiceyyyyy.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practiceyyyyy;
+
+/**
+ *
+ * @author tenes
+ */
+public class Practiceyyyyy {
+
+    /**
+     * @param args the command line arguments
+     */
+    //I love sessy
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
